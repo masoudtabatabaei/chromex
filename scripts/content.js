e96b03d590bed706a9e1f960bc91df77eb51efba
Masoud Tabatabaei <masoudtabatabaei20@gmail.com>
2023-08-13 14:59:48 +0330

    Fix: Append addlistener to every order card just once

--- scripts/content.js
+++ scripts/content.js
@@ -7,34 +7,29 @@ const handleAddInfoBtns = () => {
             const btn = document.createElement("button");
             btn.classList.add("add_info");
             btn.textContent = "Add Info";
+
+            btn.addEventListener("click", () => {
+                let parent = btn.parentElement;
+                const orderId = parent.querySelector(".orderId").textContent;
+                const orderValue = +parent.querySelector(".orderValue").textContent;
+                const paymentType = parent.querySelector(".paymentType").textContent;
+                const vehicleType = parent.querySelector(".vehicleType").textContent;
+
+                console.log({ orderId, orderValue, paymentType, vehicleType });
+
+                (async () => {
+                    chrome.storage.local.set({ orderId, orderValue, paymentType, vehicleType });
+                })();
+            });
+
             item.appendChild(btn);
         }
-    });
-}
-
-const addNewButtonOnOrderBox = () => {
-    handleAddInfoBtns();
-    let addInfoBtns = document.querySelectorAll(".add_info");
-    addInfoBtns.forEach(btn => {
-        btn.addEventListener("click", () => {
-            let parent = btn.parentElement;
-            const orderId = parent.querySelector(".orderId").textContent;
-            const orderValue = +parent.querySelector(".orderValue").textContent;
-            const paymentType = parent.querySelector(".paymentType").textContent;
-            const vehicleType = parent.querySelector(".vehicleType").textContent;
-
-            console.log({ orderId, orderValue, paymentType, vehicleType });
-
-            (async () => {
-                chrome.storage.local.set({ orderId, orderValue, paymentType, vehicleType });
-            })();
-        });
     });
 }
 
 window.addEventListener("load", () => {
     console.log("-- Loaded content.js--");
     setInterval(() => {
-        addNewButtonOnOrderBox();
+        handleAddInfoBtns();
     }, 3000);
 });
